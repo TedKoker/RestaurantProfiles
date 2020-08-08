da1fedaf932960a7516c759229cb95fddcdffffb
tedkol1992@gmail.com <tedkol1992@gmail.com>
2020-08-09 01:13:50 +0300

    AddResturant Component (mapping the final object)

--- front/src/components/restuarants/AddRestuarant.js
+++ front/src/components/restuarants/AddRestuarant.js
@@ -1,7 +1,7 @@
-import React, {useState, useEffect} from 'react'
+import React, {useState, useEffect, useRef} from 'react'
 import {compose} from 'redux'
 import {connect} from 'react-redux'
-import {reduxForm, Field, SubmissionError } from 'redux-form'
+import {reduxForm, Field, FormSection, formValues  , SubmissionError } from 'redux-form'
 import Button from 'react-bootstrap/Button'
 import Card from 'react-bootstrap/Card'
 import Spinner from 'react-bootstrap/Spinner'
@@ -11,6 +11,7 @@ import Tooltip from 'react-bootstrap/Tooltip'
 import {RequireAuth} from '../../shared/sharedLogic/HocComponents'
 import {useWindowSize} from '../../shared/sharedLogic/useFunctions'
 import * as actions from '../../actions'
+import MenuAdding from './MenuAdding'
 import '../auth/auth.scss'
 import '../../app.scss'
 
@@ -64,10 +65,37 @@ function responsiveRenderField(props) {
     )
 }
 
+function selectList(props) {
+
+    const {input} = props
+
+    const addContent =  <svg width="1em" height="1em" viewBox="0 0 16 16" className="bi bi-file-plus" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                            <path fillRule="evenodd" d="M4 1h8a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2zm0 1a1 1 0 0 0-1 1v10a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1V3a1 1 0 0 0-1-1H4z"/>
+                            <path fillRule="evenodd" d="M8 5.5a.5.5 0 0 1 .5.5v1.5H10a.5.5 0 0 1 0 1H8.5V10a.5.5 0 0 1-1 0V8.5H6a.5.5 0 0 1 0-1h1.5V6a.5.5 0 0 1 .5-.5z"/>
+                        </svg>
+
+    /**If option is selected, there will be trash can near her to delete the option */
+    return (
+        <div className="list" style={{width: "100%"}}>
+            <select size="8" name="" className="form-control" style={{padding:0}} {...input}>
+                <option disabled className="list-header">Category</option>
+                {props.values.map((val, index, array) => {
+                    return (<option key={index} value={val}>{val}</option>)
+                })}
+            </select>
+            <button>
+                {addContent}
+            </button>
+        </div>
+    )
+}
+
 function AddRestuarant(props) {
 
     const [winWidth] = useWindowSize()
     const [isResponsive, setResponsive] = useState(winWidth < 767 ? true : false)
+    const [menuObj] = useState({})
+    const categories = ["one", "two", "three"]
 
     const loadingBtn = <Spinner
                             as="span"
@@ -78,13 +106,14 @@ function AddRestuarant(props) {
                         />
 
     const onSubmit = (formProps) => {
+        /**map the menu object */
+        formProps.menu = menuObj
         console.log(formProps)
     }
 
     useEffect(() => {
         setResponsive(winWidth < 767 ? true : false)
     },[winWidth])
-
     return (
         <Card className="text-center">
             <Card.Header>Open a new restuarant</Card.Header>
@@ -120,6 +149,21 @@ function AddRestuarant(props) {
                         //validate={[required, emailPattern]}
                         direction="right"
                     />
+                    <FormSection name="menu" component={(props) => (<>{props.children}</>)}>
+                        <Field  
+                            name="category"
+                            component={selectList}
+                            //autoComplete="none"
+                            values={categories}
+                            onChange={(e) => {
+                                menuObj[e.target.value] = []
+                            }}
+                        />
+                        <MenuAdding changeFunc={(event, value, preValue, property) => {
+                            const objProp = property.replace(/(.*)\./,"")
+                            menuObj[objProp].push(value)
+                        }}/>
+                    </FormSection>
                     <Button variant="primary" size="sm" type="submit" disabled={props.submitting}>
                         {!props.submitting ? 'Add Restuarant' : loadingBtn}
                     </Button>
---
+++ front/src/components/restuarants/MenuAdding.js
@@ -0,0 +1,49 @@
+import React from 'react'
+import { Field, formValues } from 'redux-form' 
+
+function selectList(props) {
+
+    const {input} = props
+
+    const addContent =  <svg width="1em" height="1em" viewBox="0 0 16 16" className="bi bi-file-plus" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                            <path fillRule="evenodd" d="M4 1h8a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2zm0 1a1 1 0 0 0-1 1v10a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1V3a1 1 0 0 0-1-1H4z"/>
+                            <path fillRule="evenodd" d="M8 5.5a.5.5 0 0 1 .5.5v1.5H10a.5.5 0 0 1 0 1H8.5V10a.5.5 0 0 1-1 0V8.5H6a.5.5 0 0 1 0-1h1.5V6a.5.5 0 0 1 .5-.5z"/>
+                        </svg>
+
+    /**If option is selected, there will be trash can near her to delete the option */
+    return (
+        <div className="list" style={{width: "100%"}}>
+            <select size="8" name="" className="form-control" style={{padding:0}} {...input}>
+                <option disabled className="list-header">Category</option>
+                {props.values.map((val, index, array) => {
+                    return (<option key={index} value={val}>{val}</option>)
+                })}
+            </select>
+            <button>
+                {addContent}
+            </button>
+        </div>
+    )
+}
+
+function AddMenu(props) {
+    const vluse = {
+        one: ['hi, hi, hi'],
+        two: ['la', 'lal'],
+        three: ['haha', 'dsfsfdsf', 'dsfdsf', 'adaaaa']
+    }
+
+    return (
+        <>
+            <Field  
+                name={props.category ? ""+props.category : "noName"}
+                component={selectList}
+                //autoComplete="none"
+                values={props.category ? vluse[props.category] : []}
+                onChange={props.changeFunc}
+            />
+        </>
+    )
+}
+
+export default formValues("category")(AddMenu)
